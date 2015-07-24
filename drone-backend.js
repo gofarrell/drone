4f8e70183a914f5b8c31a1e76b8839f2f2336948
gofarrell <ofarrellgeorge@ymail.com>
2015-07-24 14:42:56 +0100

    Update drone-backend.js

--- drone-backend.js
+++ drone-backend.js
@@ -17,30 +17,30 @@ Cylon.robot()
 // Fly the bot
 function fly(robot) {
     bot = robot;
-    bot.nav.on("navdata", function(data) {
-        console.log(data);
-    });
+    //bot.nav.on("navdata", function(data) {
+        //console.log(data);
+    //});
     bot.drone.config('general:navdata_demo', 'TRUE');
     bot.drone.disableEmergency();
     bot.drone.ftrim();
     bot.drone.takeoff();
-    bot.drone.left(0.4);
-    after(1.85*1000, function() {
+    bot.drone.left(0.2);
+    after(0.85*1000, function() {
        bot.drone.front(0.4);
     });
-    after(4.95*1000, function() {
+    after(1.8*1000, function() {
         bot.drone.right(0.4);
     });
-    after(6.8*1000, function() {
+    after(2.65*1000, function() {
         bot.drone.back(0.4);
     });
-    after(8.55*1000, function() {
+    after(3.5*1000, function() {
         bot.drone.left(0.4);
     });
-    after(10.3*1000, function() {
+    after(3.8*1000, function() {
         bot.drone.land();
 });
-    after(12.3*1000, function() {
+    after(4.1*1000, function() {
         bot.drone.stop();
     });
 }
